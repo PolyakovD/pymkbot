a7393d0e305d1deb1b3a116f2b6a014d1f7bef5d
BakharevK <bakharev.k@gmail.com>
2017-09-21 04:59:12 +0300

    Blind strategy in progress

--- src/pymkbot/strategy/blind_strategy.py
+++ src/pymkbot/strategy/blind_strategy.py
@@ -1,13 +1,11 @@
 import datetime
+import math
 import pickle
 import random
 import time
 
-import math
-
-from pymkbot.image.async_image_provider import AsyncImageProvider
-from pymkbot.strategy.strategy import Strategy
 import pymkbot.keyboard.directkeys as keys
+from pymkbot.image.async_image_provider import AsyncImageProvider
 
 
 def random_move():
@@ -22,12 +20,12 @@ def get_random_combination():
     return random_move(), random_move(), random_attack()
 
 
-def get_random_sprint():
-    return 'RUN', random.choice(['LEFT', 'RIGHT'])
-
-
 def get_random_attack_rage():
     return random_attack(), random_attack(), random_attack()
+
+
+def get_random_sprint():
+    return 'RUN', random.choice(['LEFT', 'RIGHT'])
 
 
 def get_block():
@@ -43,16 +41,16 @@ DUMP_SIZE = 500
 
 class BlindStrategy:
     def __init__(self, player, keybd_switch, switch):
+        self._key_bindings = keys.keyboard[player]
         self.keybd_switch = keybd_switch
         self.switch = switch
 
         self.player = player
-        self._key_bindings = keys.keyboard[player]
-        self.fighting = True
+        self.fighting = False
+        self.working = False
         self.counter = 1
         self.log = []
         self.id = 0
-        self.working = False
 
     def press_moves_concurrent(self, moves, sleep_time):
         for move in moves:
@@ -78,7 +76,7 @@ class BlindStrategy:
 
     def get_players_dist(self):
         pos1, pos2 = AsyncImageProvider.get_consumer_result("position_feature")
-        return math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
+        return math.fabs(pos1[0] - pos2[0])
 
     def combo_effectiveness(self, do_something):
         opponent_hp = self.get_players_hps()[1]
@@ -86,10 +84,37 @@ class BlindStrategy:
         return opponent_hp - self.get_players_hps()[1], result
 
     def next_combo(self, distance, my_name, vs_name):
-        pass
+        all_moves = [
+            (get_random_sprint, True),
+            (get_down_block, True),
+            (get_block, True),
+            (get_random_attack_rage, False),
+            (get_random_combination, False)
+        ]
+        norm_distance = distance / 100
+        weight = [norm_distance, 1, 1, math.fabs(2 - norm_distance), norm_distance]
+        choice = self.weighted_choice(all_moves, weight)
+        move, concurrent = choice
+        if concurrent:
+            return lambda x=move(), y=0.3: self.press_moves_concurrent(x, y)
+        else:
+            return lambda x=move(), y=0.3: self.press_moves_sequence(x, y)
+
+    def weighted_choice(self, choices, weight):
+        total = sum(weight)
+        r = random.uniform(0, total)
+        upto = 0
+        for c, w in zip(choices, weight):
+            if upto + w >= r:
+                return c
+            upto += w
+        assert "WTF, rand doesn't work"
 
     def prepare(self):
         self.counter = 1
+        with open('blind_bot_log.pickle', 'rb') as file:
+            self.log = pickle.load(file)
+        self.id = self.log[:-1]['id'] + 1
 
     def stop(self):
         self.fighting = False
